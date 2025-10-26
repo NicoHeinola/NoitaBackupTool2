06b7d94fa37422a544315d8b004aca55a4d7a942
Nico Heinola <nico.heinola@gmail.com>
2025-10-26 15:22:31 +0200

    Refactor index.vue layout for improved structure and update floating button color

--- ui/frontend/src/pages/index.vue
+++ ui/frontend/src/pages/index.vue
@@ -2,7 +2,17 @@
 
 <template>
   <v-container class="w-100 h-100">
-    <backups-table />
+    <v-row>
+      <v-col cols="12">
+        <h1>Backups</h1>
+      </v-col>
+      <v-col class="d-flex align-center justify-end" cols="12">
+        <v-btn prepend-icon="mdi-plus">Add new</v-btn>
+      </v-col>
+      <v-col cols="12">
+        <backups-table />
+      </v-col>
+    </v-row>
   </v-container>
-  <floating-button color="primary" icon="mdi-cog" />
+  <floating-button color="secondary" icon="mdi-cog" />
 </template>
